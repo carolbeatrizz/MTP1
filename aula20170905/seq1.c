0994dc2d876c26cd06b662bedadbf0bdc916c80a
carolbeatrizz <31655964+carolbeatrizz@users.noreply.github.com>
2017-12-07 23:12:07 -0200

    fim de aula

--- aula20170905/seq1.c
+++ aula20170905/seq1.c
@@ -1,14 +1,14 @@
-#include <iostream>
-#include <stdio.h>
-using namespace std;
 
-void main()
+#include <stdio.h>
+
+
+int main()
 {
 int l,area;
 printf("Digite o valor do lado do quadrado:");
 scanf("%d",&l);
 area=l*l;
 scanf("O valor da area do quadrado vale:%f",area);
-system("pause");
+return 0;
 }
 
